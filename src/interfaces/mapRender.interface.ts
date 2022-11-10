38c787f6732ee9b529aab7243b353adb1087838a
Sam Berry <sam.berry@gmail.com>
2022-11-10 22:46:32 +0000

    Sort triangles by height

---
+++ src/interfaces/mapRender.interface.ts
@@ -0,0 +1,10 @@
+export interface RenderTriangle {
+  color: string;
+  coordinates: Coordinate[];
+  heighestPoint: number;
+}
+
+export interface Coordinate {
+  x: number;
+  y: number;
+}
--- src/services/renderMap/renderMap.ts
+++ src/services/renderMap/renderMap.ts
@@ -1,6 +1,9 @@
 import { createCanvas } from 'canvas';
 import { existsSync, mkdirSync, writeFileSync } from 'fs';
 import { MapData } from '../../interfaces/mapData.interface';
+import { RenderTriangle } from '../../interfaces/mapRender.interface';
+
+const worldSpaceToPixelScale = 4;
 
 export const renderMap = (mapData: MapData) => {
   const width = 2500;
@@ -15,6 +18,20 @@ export const renderMap = (mapData: MapData) => {
   ctx.fillStyle = '#fff';
   ctx.strokeStyle = '#fff';
   ctx.lineWidth = 1;
+
+  const trisToRender = trianglesToRender(mapData);
+  trisToRender.sort((a, b) => a.heighestPoint - b.heighestPoint);
+
+  trisToRender.forEach((tri) => {
+    drawTriangle(tri, ctx);
+  });
+
+  const fileBuffer = canvas.toBuffer('image/png');
+  writeFile(fileBuffer, mapData.MapProperties.mapTitle);
+};
+
+const trianglesToRender = (mapData: MapData): RenderTriangle[] => {
+  const trisToRender: RenderTriangle[] = [];
 
   mapData.Brushes.forEach((Brush) => {
     if (mapData.Materials[Brush.material].startsWith('Skybox/')) {
@@ -32,22 +49,25 @@ export const renderMap = (mapData: MapData) => {
     if (Brush.mapDraw === 'False') {
       return;
     }
+
     Brush.Faces.forEach((face) => {
-      ctx.fillStyle = mapData.Colors[face.color].value;
       const tris = splitTris(face.tris);
       const verts = splitVerts(face.verts);
       tris.forEach((tri) => {
-        ctx.beginPath();
-        ctx.moveTo(verts[tri[0]][0] * 4, verts[tri[0]][2] * 4);
-        ctx.lineTo(verts[tri[1]][0] * 4, verts[tri[1]][2] * 4);
-        ctx.lineTo(verts[tri[2]][0] * 4, verts[tri[2]][2] * 4);
-        ctx.closePath();
-        ctx.fill();
+        trisToRender.push({
+          color: mapData.Colors[face.color].value,
+          coordinates: [
+            { x: verts[tri[0]][0] * worldSpaceToPixelScale, y: verts[tri[0]][2] * worldSpaceToPixelScale },
+            { x: verts[tri[1]][0] * worldSpaceToPixelScale, y: verts[tri[1]][2] * worldSpaceToPixelScale },
+            { x: verts[tri[2]][0] * worldSpaceToPixelScale, y: verts[tri[2]][2] * worldSpaceToPixelScale },
+          ],
+          heighestPoint: HeighestVert(verts),
+        });
       });
     });
   });
-  const fileBuffer = canvas.toBuffer('image/png');
-  writeFile(fileBuffer, mapData.MapProperties.mapTitle);
+
+  return trisToRender;
 };
 
 const splitTris = (tris: string): number[][] => {
@@ -70,4 +90,19 @@ const writeFile = (fileBuffer: Buffer, mapName: string) => {
     mkdirSync(outputDirectory);
   }
   writeFileSync(`${outputDirectory}/${mapName}.png`, fileBuffer);
+};
+
+const HeighestVert = (verts: number[][]) => {
+  const zIndexs = verts.map((vert) => vert[1]);
+  return Math.max.apply(null, zIndexs);
+};
+
+const drawTriangle = (tri: RenderTriangle, ctx: CanvasRenderingContext2D) => {
+  ctx.fillStyle = tri.color;
+  ctx.beginPath();
+  ctx.moveTo(tri.coordinates[0].x, tri.coordinates[0].y);
+  ctx.lineTo(tri.coordinates[1].x, tri.coordinates[1].y);
+  ctx.lineTo(tri.coordinates[2].x, tri.coordinates[2].y);
+  ctx.closePath();
+  ctx.fill();
 };
