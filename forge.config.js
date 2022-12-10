5708d73654613de7e66e45f483b150cdb856a083
Sam Berry <sam.berry@gmail.com>
2022-12-10 12:02:55 +0000

    Adds app icon

---
+++ forge.config.js
@@ -0,0 +1,30 @@
+path = require('node:path');
+
+module.exports = {
+  packagerConfig: {
+    icon: path.join(__dirname, '/assets/icons/icon.ico'),
+    ignore: [
+      'src-electron',
+      'src-renderer',
+      '.editorconfig',
+      '.eslintignore',
+      '.eslintrc.yml',
+      '.gitattributes',
+      '.gitignore',
+      '.prettierrc.yml',
+      'README.md',
+      'tsconfig.base.json',
+      'tsconfig.electron.json',
+      'tsconfig.renderer.json',
+    ],
+  },
+  makers: [
+    {
+      name: '@electron-forge/maker-zip',
+      config: {
+        name: 'electron_quick_start',
+        setupIcon: path.join(__dirname, './assets/icons/icon.ico'),
+      },
+    },
+  ],
+};
